251e56a2f985866f2586979822f2a3045cd3b65c
Ivan Obykhvost <ivan.obykhvost@nure.ua>
2016-10-09 16:35:42 +0300

    Initial commit for usermanagement

---
+++ usermanagement/src/test/java/kn_14_5/obykhvost/usermanagement/UserTest.java
@@ -0,0 +1,35 @@
+package kn_14_5.obykhvost.usermanagement;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Calendar;
+import java.util.Date;
+
+public class UserTest {
+	private User user;
+	private Date dateOfBirth;
+
+	@Before
+	public void setUp() throws Exception { 
+		user = new User();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1984, Calendar.MAY, 26);
+		dateOfBirth = calendar.getTime();
+	}
+	
+	@Test
+	public void testGetFullName() {
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		assertEquals("Doe, John", user.getFullName());
+	}
+	
+	@Test
+	public void testGetDateOfBirth()
+	{
+		user.setDateOfBirth(dateOfBirth);
+		assertEquals(2016 - 1984, user.getAge());
+	}
+
+}
